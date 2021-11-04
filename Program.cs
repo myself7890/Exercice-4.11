01ead6834033b02978686087448575dea16f87a8
Julie Laporte <laporte.julie@gmail.com>
2021-11-04 09:41:13 -0400

    fix

--- Program.cs
+++ Program.cs
@@ -14,13 +14,14 @@ namespace Exercice_4._11
             int compteurNote;
             double sommeRésultat = 0;
 
-            for (compteurNote = 1; compteurNote <= nbDeNotes; compteurNote++)
+            for (compteurNote = 0; compteurNote < nbDeNotes; compteurNote++)
             {
                 sommeRésultat += LireNièmeNote();
             }
             return sommeRésultat / nbDeNotes;
 
         }
+        
         static double LireNièmeNote()
         {
             const int Min = 0;
@@ -31,17 +32,17 @@ namespace Exercice_4._11
             {
                 Console.Write("Entrez la note [0,100]: ");
                 note = double.Parse(Console.ReadLine());
-
             } while (note < Min || note > Max);
 
             return note;
 
         }
+
         static void Main(string[] args)
         {
             const int NombreEtudiants = 2;
 
-            for(int compteur = 1; compteur <= NombreEtudiants; compteur++)
+            for (int compteur = 0; compteur < NombreEtudiants; compteur++)
             {
                 double moyenne = ObtenirNotesNièmeEtudiant();
                 Console.Write(moyenne);
